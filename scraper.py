14ad01c56c6cfedfbc1fd9e42c3ae49a56acfb2c
Michael Markieta <michael@letalone.ca>
2022-11-09 00:11:19 -0500

    remove timeout; listen on all interfaces

--- scraper.py
+++ scraper.py
@@ -94,4 +94,4 @@ if __name__ == "__main__":
     print("Running initialization code...")
     clinicFeatureCollection = scrape()
     print("Starting server...")
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
